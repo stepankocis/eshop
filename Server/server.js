43b281f89f37570c7716aa6c130f17a309186c5e
otep <stepan.kocis@educanet.cz>
2026-02-24 10:53:07 +0100

    server or smth

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
-let data = [];
+let data = [
+    {
+        name:"NaN",
+        age: -1
+    }
+];
 app.use(express.json());
 app.get("/api/users", (request, response) => {
     response.json(data);
 });
+
+app.post("/api/users", (request, response) => {
+    if(request.body.name === "Samuel Kodytek") {
+        response.status(403).json("https://samjenegr.eu/");
+    } else {
+        console.log(request.body);
+        data.push(request.body);
+        response.status(201).json("Uživatel vytvořen");
+    }
+})
+
 app.listen(3002);
